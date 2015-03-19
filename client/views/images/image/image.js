1b590c01b3cff240ed786bf49a9bf281d6afbcf5
Praxie <mr.praxie@gmail.com>
2015-03-19 19:36:17 +0200

    Initial commit

---
+++ client/views/images/image/image.js
@@ -0,0 +1,12 @@
+Template.Image.events({
+});
+
+Template.Image.helpers({
+});
+
+
+Template.Image.rendered = function () {
+};
+
+Template.Image.created = function () {
+};
--- client/views/images/images.js
+++ client/views/images/images.js
@@ -1,37 +1,15 @@
 Template.Images.events({
-
-  'click .category': function (e, tmpl) {
-    e.preventDefault();
-    tmpl.category.set($(e.target).text());
-  },
-
-  'click .menu > h1': function (e, tmpl) {
-    tmpl.category.set('');
-  }
-
 });
 
 Template.Images.helpers({
-  images: function (limit) {
-    return Images.find({category: Template.instance().category.get()}, {limit: limit});
+  images: function () {
+    return Images.find({category: this.category}, {limit: this.limit});
   }
 });
 
 
-// Template.Images.rendered = function () {
-//   console.log('Images are rendered!');
-// };
+Template.Images.rendered = function () {
+};
 
 Template.Images.created = function () {
-  this.category = new ReactiveVar('');
-};
-
-// Image
-
-Template.Image.created = function () {
-  console.log('Image is created!');
-};
-
-Template.Image.rendered = function () {
-  console.log('Image is rendered!');
 };
